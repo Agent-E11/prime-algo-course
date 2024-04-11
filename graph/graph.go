9e66f482c5817419cf5052602b69159f3d1a1c7b
Eamon Burns (Laptop WSL) <eamon.r.burns@gmail.com>
2024-04-10 22:05:11 -0700

    Implement Breadth First Search for a Weighted Adjacency Matrix

--- graph/graph.go
+++ graph/graph.go
@@ -1,1 +1,88 @@
 package graph
+
+import (
+	"fmt"
+	"slices"
+)
+
+// Weighted Adjacency Matrix
+type WAM = [][]int
+
+func NewWAM(n int) WAM {
+	g := make([][]int, n)
+	for i := range n {
+		g[i] = make([]int, n)
+	}
+	return g
+}
+
+// Breadth First Search
+func BFSearch(graph WAM, source int, needle int) []int {
+	seen := make([]bool, len(graph))
+
+	prev := make([]int, len(graph))
+
+	for i := range prev { prev[i] = -1 }
+
+	seen[source] = true
+	q := []int{source}
+
+	for {
+		fmt.Printf("seen: %v\n", seen)
+		fmt.Printf("prev: %v\n", prev)
+		fmt.Printf("q: %v\n", q)
+
+		curr := q[0]
+		q = q[1:]
+
+		if curr == needle {
+			break
+		}
+
+		cons := graph[curr]
+		for other, con := range cons {
+			if con == 0 { continue }
+			if seen[other] { continue }
+
+			seen[other] = true
+			prev[other] = curr
+
+			q = append(q, other)
+		}
+
+		if len(q) <= 0 { break }
+	}
+
+	// Build the path
+
+	fmt.Println("starting at needle:", needle)
+	curr := needle
+	path := []int{}
+	
+	for prev[curr] != -1 {
+		fmt.Printf("curr: %v\n", curr)
+		path = append(path, curr)
+		curr = prev[curr]
+	}
+
+	fmt.Printf("path: %v\n", path)
+
+	if len(path) > 0 {
+		fmt.Println("Reversing and returning path")
+		path = append(path, source)
+
+		slices.Reverse(path)
+		return path
+	}
+	return []int{}
+}
+
+func PrintWAM(graph WAM) {
+	for self, cons := range graph {
+		for other, con := range cons {
+			if con != 0 {
+				fmt.Printf("%d --%d-> %d\n", self, con, other)
+			}
+		}
+	}
+}
--- main.go
+++ main.go
@@ -3,18 +3,31 @@ package main
 import (
 	"fmt"
 
-	"github.com/agent-e11/prime-algo-course/heap"
+	"github.com/agent-e11/prime-algo-course/graph"
 )
 
 func main() {
-	h := heap.New[int]()
+	g := graph.NewWAM(5)
 
-	for _, i := range []int{8, 2, 3, 7, 4, 0, 2, 9, 7, 5, 4, 0, 3, 4, 8, 0, 6, 4, 9, 2, 3, 4, 9, 2, 3, 4, 8, 0, 1, 0, 1} {
-		h.Insert(i)
-		h.Print()
+	g[3][1] = 1
+	g[1][2] = 2
+	g[2][4] = 3
+	g[0][4] = 4
+
+	graph.PrintWAM(g)
+
+	path := graph.BFSearch(g, 3, 4)
+
+	fmt.Printf("path: %v\n", path)
+
+	fmt.Println("Total weight")
+
+	curr := path[0]
+	weight := 0
+	for i := 1; i < len(path); i++ {
+		weight += g[curr][path[i]]
+		curr = path[i]
 	}
 
-	fmt.Println(h.Delete())
-
-	h.Print()
+	fmt.Printf("weight: %v\n", weight)
 }
